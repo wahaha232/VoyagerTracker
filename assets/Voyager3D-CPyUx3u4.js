066a05b5c455d9ed3d446e49aafacbeda23db32c
wahaha232 <wahaha232@users.noreply.github.com>
2026-09-25 18:06:25 +0800

    Updates

--- assets/Voyager3D-CPyUx3u4.js
+++ assets/Voyager3D-CPyUx3u4.js
@@ -1,4 +1,4 @@
-import{c as Ay,g as Cy,R as Ry,d as pE,r as Fe,j as Le}from"./main-C16CClvW.js";/**
+import{c as Ay,g as Cy,R as Ry,d as pE,r as Fe,j as Le}from"./main-Bd-ewuj_.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
